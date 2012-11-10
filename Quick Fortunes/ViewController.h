7b5479e51dff9c83d666097fa8b5726006750bff
Patrick Serrano <hello@patrickserrano.com>
2012-11-10 02:21:32 -0500

    Initial Commit

---
+++ Quick Fortunes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quick Fortunes
+//
+//  Created by Patrick Serrano on 11/10/12.
+//  Copyright (c) 2012 Patrick Serrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
